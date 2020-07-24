246d121e11888a88e8f585d7d582c0f2996f922e
gleixer <gleixer.sherrida@gmail.com>
2020-07-24 18:56:04 -0400

    Add files via upload

--- DT Code/uPNC-Mentor-2020/DT Code/Problem 4/Problem4_12.m
+++ DT Code/uPNC-Mentor-2020/DT Code/Problem 4/Problem4_12.m
@@ -1,5 +1,5 @@
-% function problem4_12(data)
-clc; close all;
+% function Problem4_12(Data)
+clc;
 out = pmdDataSetup(Data);
 firingRates = out(:,2:end);
 numN = size(firingRates, 2);
@@ -7,37 +7,44 @@ numTrials = size(firingRates,1);
 %%
 [spike, pos, velo] = PosVeloTime(Data);
 
+%%
+X = [];
+y = []; 
+xTest = [];
+yTest = [];
+plotXpos = [];
+plotYpos = [];
+for i=1:numTrials
+    temp = [pos.x{i}(1:end-1) pos.y{i}(1:end-1) velo.x{i}(1:end-1) velo.y{i}(1:end-1)];
+    if (max(max(isnan(temp)))==1)
+        continue;
+    end
+    if i <= floor(numTrials*0.8)
+%             X = spike{i}(bin,:);
+%             y = [pos.x{i}(1:end-1) pos.y{i}(1:end-1) velo.x{i}(1:end-1) velo.y{i}(1:end-1)];
+%             result =(X' *X)^(-1)*(X'*y);
+            result = regress([pos.x{i}(1:end-1) pos.y{i}(1:end-1) velo.x{i}(1:end-1) velo.y{i}(1:end-1)], spike{i});
 
-posX = [];
-posY = [];
-veloX = [];
-veloY = []; 
-toggle = false;
-for i=1:1222
-        if i== 1
-            X = [spike{i}];
-            y= [pos.x{i} pos.y{i} velo.x{i} velo.y{i}];
-        else
-            X = [X;spike{i}];
-            posX = [posX;pos.x{i}];
-            posY = [posY;pos.y{i}];
-            veloX = [veloX;velo.x{i}];
-            veloY = [veloY;velo.y{i}]; 
+%             predictedTraj = (yTest*(result)')/50;
+        
 
-        end
+%         X = [X;spike{i}];
+%         y= [y;temp];
+    else
+%         xTest = [xTest;spike{i}];
+%         yTest = [yTest;temp];
+%         plotXpos = [plotXpos;pos.x{i}(:)];
+%         plotYpos = [plotYpos;pos.y{i}(:)];
+    end
+    
 end
 
-y = [posX posY veloX veloY];
-%%
-index = floor(.8*numTrials);
-trainX = X(1:index, :);
-trainY= y(1:index,:);
-testX = X(index+1:end,:);
-testY= y(index+1:end,:);
-% result = regress(trainY, trainX);
-result =(X' *X)\X'*y;
-AngularError= abs((testX*result)-testY);
-figure;
-histogram(AngularError)
-title('Histogram of the Absolute Angular Error')
 
+%%
+result = regress(y, X);
+result =(X' *X)^(-1)*(X'*y);
+predictedTraj = (yTest*(result)')/50;
+plot(plotXpos, plotYpos)
+hold on
+plot(predictedTraj(:,3), predictedTraj(:,4), 'Color', 'r')
+hold off
